b6b81b3846dcd5c3f9478f3b15d81052727c1201
dishant987 <tankdishant2003@gmail.com>
2024-09-14 23:29:53 +0530

    change

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -9,9 +9,9 @@ const NavItems = () => {
       {
         navLinks.map((link) => (
           <li key={link.id} className='nav-li'>
-            <Link onClick={() => { }} to={link.href} className='nav-li_a'>
+            <a onClick={() => { }} href={link.href} className='nav-li_a'>
               {link.name}
-            </Link>
+            </a>
           </li>
         ))
       }
@@ -25,10 +25,10 @@ const Navbar = () => {
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className=' max-w-7xl mx-auto'>
         <div className=' flex justify-between items-center py-5 mx-auto c-space'>
-          <Link to={'/'} className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
+          <a href={'/'} className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
             Dishant
-          </Link>
-         
+          </a>
+
           <button className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle menu' onClick={() => setIsOpen((prev) => !prev)}>
             <img src={isOpen ? "assets/menu.svg" : "assets/close.svg"} alt="toggle" className='w-6 h-6' />
           </button>
