368687ded1e05329a0c1e08fc27711a1c0b4f894
dishant987 <tankdishant2003@gmail.com>
2024-09-17 12:54:04 +0530

    btn

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,12 +2,58 @@ import { lazy, Suspense, useState } from 'react';
 
 const Globe = lazy(() => import('react-globe.gl'));
 import Button from '../components/Button.jsx';
-import { HoverBorderGradient } from '@/components/ui/hover-border-gradient.jsx';
-import ShinyButton from '@/components/magicui/shiny-button.jsx';
+import styled from 'styled-components';
 const IconCloud = lazy(() => import('@/components/magicui/icon-cloud.jsx'));
 const GlobeSkeleton = () => (
   <div className="rounded-full bg-gray-500 w-[326px] h-[326px] animate-pulse"></div>
 );
+
+
+
+
+
+const StyledWrapper = styled.div`
+  /* Dark Mode 3D Button */
+button {
+  font: inherit;
+  background-color: #2a2a2a;
+  border: 0;
+  color: #e0e0e0;
+  border-radius: 0.5em;
+  font-size: 1.35rem;
+  padding: 0.375em 1em;
+  font-weight: 600;
+  text-shadow: 0 0.0625em 0 #000;
+  box-shadow:
+    inset 0 0.0625em 0 0 #3a3a3a,
+    0 0.0625em 0 0 #252525,
+    0 0.125em 0 0 #202020,
+    0 0.25em 0 0 #1c1c1c,
+    0 0.3125em 0 0 #181818,
+    0 0.375em 0 0 #141414,
+    0 0.425em 0 0 #101010,
+    0 0.425em 0.5em 0 #0a0a0a;
+  transition: 0.15s ease;
+  cursor: pointer;
+}
+
+button:active {
+  translate: 0 0.225em;
+  box-shadow:
+    inset 0 0.03em 0 0 #3a3a3a,
+    0 0.03em 0 0 #252525,
+    0 0.0625em 0 0 #202020,
+    0 0.125em 0 0 #1c1c1c,
+    0 0.125em 0 0 #181818,
+    0 0.2em 0 0 #141414,
+    0 0.225em 0 0 #101010,
+    0 0.225em 0.375em 0 #0a0a0a;
+}
+
+`;
+
+
+
 const SkeletonIconCloud = () => {
   return (
     <div className="skeleton-icon-cloud grid grid-cols-4 gap-2">
@@ -26,7 +72,7 @@ const slugs = [
   "sql",
   "shadcn",
   "materialui",
-  
+
   "react",
   "nextjs",
   "php",
@@ -155,7 +201,10 @@ const About = () => {
                 <p className="lg:text-base md:text-xl font-medium text-gray_gradient text-white">infodishant04tank@gmail.com</p>
               </div>
               <div className='flex justify-center items-center pt-4'>
-                <ShinyButton OnClick={downloadResume} text=' Download Resume' className={'px-7 py-3 rounded-3xl'} />
+                {/* <ShinyButton OnClick={downloadResume} text=' Download Resume' className={'px-7 py-3 rounded-3xl'} /> */}
+                <StyledWrapper>
+                  <button onClick={downloadResume}>Resume</button>
+                </StyledWrapper>
               </div>
             </div>
           </div>
